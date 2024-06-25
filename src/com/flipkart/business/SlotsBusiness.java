09c52d7d47d9024dee620a63d8d08c64b15754d8
Akkkanksha <akankshashah747@gmail.com>
2024-06-25 20:44:06 +0530

    Slots Business + Client

---
+++ src/com/flipkart/business/SlotsBusiness.java
@@ -0,0 +1,13 @@
+package com.flipkart.business;
+
+public class SlotsBusiness {
+
+    public boolean updateAvailability() {
+        System.out.println("Updating Slot Availability");
+        return true;
+    }
+
+    public void getSlotDetails() {
+        System.out.println("Getting Slot Details");
+    }
+}
