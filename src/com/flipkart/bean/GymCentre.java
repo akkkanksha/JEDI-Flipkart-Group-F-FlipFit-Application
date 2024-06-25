ff3d288e8b1302639e9bca2e70bccd0b7af35158
pan <pragyapandeystzf@gmail.com>
2024-06-25 20:09:15 +0530

    bean finished

--- src/com/flipkart/bean/GymCentre.java
+++ src/com/flipkart/bean/GymCentre.java
@@ -1,5 +1,33 @@
 package com.flipkart.bean;
 
-public class GymCentre {
+public class GymCentre extends AbstractUser{
+    private String PAN;
+    private String Aadhaar;
+
+    public String getPAN() {
+        return PAN;
+    }
+
+    public void setPAN(String PAN) {
+        this.PAN = PAN;
+    }
+
+    public String getAadhaar() {
+        return Aadhaar;
+    }
+
+    public void setAadhaar(String aadhaar) {
+        Aadhaar = aadhaar;
+    }
+
+    public String getGSTIN() {
+        return GSTIN;
+    }
+
+    public void setGSTIN(String GSTIN) {
+        this.GSTIN = GSTIN;
+    }
+
+    private String GSTIN;
 
 }
