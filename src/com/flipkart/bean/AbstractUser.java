d400ebff253d9069dadf57e8218328cdeb25900e
pan <pragyapandeystzf@gmail.com>
2024-06-25 19:53:43 +0530

    Abstract1.0

---
+++ src/com/flipkart/bean/AbstractUser.java
@@ -0,0 +1,15 @@
+package com.flipkart.bean;
+
+public class AbstractUser {
+    private int userId;
+    private String userName;
+    private String password;
+    private String emailID;
+    private String phoneNumber;
+    private String city;
+    private String pinCode;
+    // Role: 0 -> customer, 1 -> gym owner
+    private int role;
+
+    
+}
