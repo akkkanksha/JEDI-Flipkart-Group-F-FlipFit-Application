01820caaa42afd62201c3bc9544c9cb78227a2a5
i-am-karan <karankumar1082002@gmail.com>
2024-06-25 21:46:54 +0530

    minor changes

--- src/com/flipkart/business/AdminUserBusiness.java
+++ src/com/flipkart/business/AdminUserBusiness.java
@@ -11,19 +11,23 @@ public class AdminUserBusiness {
         this.adminUser=adminUser;
     }
     public boolean adminLogin(AdminUser adminUser){
+        System.out.println("AdminUserBusiness.adminLogin");
         return true;
     }
     public List<Object> getPendingOwnerList(){
-
+        System.out.println("AdminUserBusiness.getPendingOwnerList");
         return new ArrayList<Object>();
     }
     public List<Object> getUserList(){
+        System.out.println("AdminUserBusiness.getUserList");
         return new ArrayList<Object>();
     }
-    public boolean validateOwner(){
+    public boolean validateOwner(int ownerId){
+        System.out.println("AdminUserBusiness.validateOwner "+ownerId);
         return true;
     }
-    public boolean deleteOwner(){
+    public boolean deleteOwner(int ownerId){
+        System.out.println("AdminUserBusiness.deleteOwner" + ownerId);
         return true;
     }
 
