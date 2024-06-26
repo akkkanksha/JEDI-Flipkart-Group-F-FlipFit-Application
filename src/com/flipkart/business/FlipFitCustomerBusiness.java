df952995acac3e2578d3fa94886aaa7086500c87
DEEPENDRA SINGH <87956562+deependrasingh1037@users.noreply.github.com>
2024-06-26 11:22:10 +0530

    Update FlipFitCustomerBusiness.java

--- src/com/flipkart/business/FlipFitCustomerBusiness.java
+++ src/com/flipkart/business/FlipFitCustomerBusiness.java
@@ -1,6 +1,6 @@
 package com.flipkart.business;
-
-public class FlipFitCustomerBusiness {
+import com.flipkart.business.interfaces.IFlipFitCustomer;
+public class FlipFitCustomerBusiness extends  {
 	public void viewBookedSlots(int userId) {
         System.out.println("Viewing booked slots:> ");
     }
