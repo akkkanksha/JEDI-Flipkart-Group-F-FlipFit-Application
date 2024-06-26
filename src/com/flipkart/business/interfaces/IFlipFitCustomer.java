51f4bfa6d154bb2f8ca8e00ec0818f9375233ae7
DEEPENDRA SINGH <87956562+deependrasingh1037@users.noreply.github.com>
2024-06-26 11:32:33 +0530

    Update IFlipFitCustomer.java

--- src/com/flipkart/business/interfaces/IFlipFitCustomer.java
+++ src/com/flipkart/business/interfaces/IFlipFitCustomer.java
@@ -3,6 +3,6 @@ public interface IFlipFitCustomer {
     public void viewBookedSlots(int userId);
     public boolean checkBookingConflicts(int userId, String slot);
     public void viewCentres();
-    public boolean makePayment(int userId)
+    public boolean makePayment(int userId);
     public boolean editDetails(int userId);
 }
