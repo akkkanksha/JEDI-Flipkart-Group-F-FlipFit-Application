07b43f18a7f2e083608a73f7ce7bdce5f07f9e42
deependra.singh2 <deependra.singh2@FK1089-215019L.local>
2024-06-26 15:39:34 +0530

    options updated

--- src/com/flipkart/client/GymFlipFitCustomerMenu.java
+++ src/com/flipkart/client/GymFlipFitCustomerMenu.java
@@ -6,17 +6,43 @@ import com.flipkart.business.GymCentreBusiness;
 import com.flipkart.business.SlotsBusiness;
 import com.flipkart.business.interfaces.ISlots;
 
+import java.util.Scanner;
+
 public class GymFlipFitCustomerMenu {
     public static String slot = "6am-7am";
     static FlipFitCustomerBusiness service = new FlipFitCustomerBusiness();
+    static SlotsBusiness slots = new SlotsBusiness();
     static int userId = 101;
 
     public static void getFlipFitCustomerMenu() {
-        service.viewBookedSlots(userId);
+        System.out.println("Enter your choice : ");
+        System.out.println("1. View Booked Slots");
+        System.out.println("2. View User Profile");
+        System.out.println("3. View Centres");
+        Scanner sc=new Scanner(System.in);
+        int choice = sc.nextInt();
+
+        switch (choice){
+            case 1:
+                service.viewBookedSlots(userId);
+            case 2:
+                service.editDetails(userId);
+            case 3:{
+                service.viewCentres();
+                System.out.println("Enter centreId : ");
+                int centreId = sc.nextInt();
+                slots.getSlotDetails();
+                System.out.println("Enter slot number : ");
+                int slotNumber = sc.nextInt();
+                slots.updateAvailability();
+
+                System.out.println("Successfully booked a slot with centreId : " + centreId + " and slot number : " + slotNumber);
+            }
+        }
+
 //        service.checkBookingConflicts(userId, slot);
-        service.viewCentres();
 //        service.makePayment(userId);
-        service.editDetails(userId);
+
     }
     public static void getFlipFitBookingMenu() {
         BookingsBusiness BBservice = new BookingsBusiness();
