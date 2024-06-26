47e898ea996ec2ccbecf24b5be1a71d976c53db1
pan <pragyapandeystzf@gmail.com>
2024-06-26 10:10:15 +0530

    added comments

--- src/com/flipkart/client/FlipFitClientApp.java
+++ src/com/flipkart/client/FlipFitClientApp.java
@@ -6,6 +6,18 @@ import com.flipkart.business.GymCentreBusiness;
 
 public class FlipFitClientApp {
     public static void main(String[] args) {
+
+        // Gym Owner
+
+        // Customer
+
+        // Admin
+
+        // Bookings
+
+        // Slots
+
+        // Gym Centre
         GymCentreBusiness GCBservice = new GymCentreBusiness();
         GCBservice.createGymCentre();
         System.out.println("Update Gym Centre" + GCBservice.updateGymCentre());
