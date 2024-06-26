5f5b22d9166d5acb934242193376b49b256df899
pan <pragyapandeystzf@gmail.com>
2024-06-26 11:06:38 +0530

    added gymcentre interface

--- src/com/flipkart/business/interfaces/IGymCentre.java
+++ src/com/flipkart/business/interfaces/IGymCentre.java
@@ -1,19 +1,9 @@
 package com.flipkart.business.interfaces;
 
 public interface IGymCentre {
-    public void createGym(String gymName);
-    public boolean updateGymCentre()
-
-    public boolean deleteGymCentre(){
-        System.out.println("Deleting Gym Centre:> ");
-        return true;
-    }
-
-    public void listGymCentres(){
-        System.out.println("Listing Gym Centres:> ");
-    }
-
-    public void viewAvailableSlots() {
-        System.out.println("Viewing Available Slots:> ");
-    }
+    public void createGymCentre();
+    public boolean updateGymCentre();
+    public boolean deleteGymCentre();
+    public void listGymCentres();
+    public void viewAvailableSlots();
 }
