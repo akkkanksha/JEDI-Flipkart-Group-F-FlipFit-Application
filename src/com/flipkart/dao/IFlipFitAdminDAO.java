aa9845786e29d23ddad9d8ca7e97f7c783265c91
i-am-karan <karankumar1082002@gmail.com>
2024-06-26 23:17:59 +0530

    added AdminDAO

--- src/com/flipkart/dao/IFlipFitAdminDAO.java
+++ src/com/flipkart/dao/IFlipFitAdminDAO.java
@@ -1,4 +1,13 @@
 package com.flipkart.dao;
 
+import com.flipkart.bean.AdminUser;
+
+import java.util.List;
+
 public interface IFlipFitAdminDAO {
+    public boolean adminLogin(AdminUser adminUser);
+    public List<Object> getPendingOwnerList();
+    public List<Object> getUserList();
+    public boolean validateOwner(int ownerId);
+    public boolean deleteOwner(int ownerId);
 }
