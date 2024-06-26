b7d320f49a272b72c00f1a814d341abf04e93785
deependra.singh2 <deependra.singh2@FK1089-215019L.local>
2024-06-26 10:08:06 +0530

    updating client file

--- src/com/flipkart/business/FlipFitCustomerBusiness.java
+++ src/com/flipkart/business/FlipFitCustomerBusiness.java
@@ -2,18 +2,18 @@ package com.flipkart.business;
 
 public class FlipFitCustomerBusiness {
 	public void viewBookedSlots(int userId) {
-		
+		System.out.println("Here are all the slots that you booked for today -->");
     }
-    public void checkBookingConflicts(int userId) {
-    	
+    public void checkBookingConflicts(int userId, int startTime) {
+		System.out.println("Checking any conflicts -->");	
     }
     public void viewCentres(int userId) {
-    	
+		System.out.println("Here are all the centres available in your city -->");	    	
     }
     public void makePayment(int userId) {
-
+		System.out.println("Here are the details of the payment you made -->");	
     }
     public void editDetails(int userId) {
-    	
+		System.out.println("Enter the updated details -->");	
     }
 }
--- src/com/flipkart/client/FlipFitCustomerClientApp.java
+++ src/com/flipkart/client/FlipFitCustomerClientApp.java
@@ -1,5 +1,16 @@
 package com.flipkart.client;
+import com.flipkart.business.FlipFitCustomerBusiness;
 
 public class FlipFitCustomerClientApp {
-
+	public static void main() {
+		FlipFitCustomerBusiness service = new FlipFitCustomerBusiness();
+		int userId=101;
+		int startTime=7;
+		service.viewBookedSlots(userId);
+		service.checkBookingConflicts(userId,startTime);
+		service.viewCentres(userId);
+		service.makePayment(userId);
+		service.editDetails(userId);
+	}
+	
 }
