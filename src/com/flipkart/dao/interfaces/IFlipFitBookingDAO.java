589fc348b4cc7a87856f8bc5ab10de11b9f36ada
isha.rawat <isharawat2142@gmail.com>
2024-06-28 00:58:53 +0530

    Removed errors

--- src/com/flipkart/dao/interfaces/IFlipFitBookingDAO.java
+++ src/com/flipkart/dao/interfaces/IFlipFitBookingDAO.java
@@ -7,7 +7,5 @@ import java.util.List;
 public interface IFlipFitBookingDAO {
     public void makeBooking(FlipFitBooking booking);
     boolean deleteBooking(int booking);
-    public List<FlipFitBooking> getAllBooking(String userId);
-
-    public List<FlipFitBooking> getAllBookings(String userId);
+    public List<FlipFitBooking> getAllBookings(int userId);
 }
