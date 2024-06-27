9113a05a69ee218f0f936c2acd0217e1b263a543
DEEPENDRA SINGH <87956562+deependrasingh1037@users.noreply.github.com>
2024-06-27 10:19:31 +0530

    Update GymCentreBusiness.java

--- src/com/flipkart/business/GymCentreBusiness.java
+++ src/com/flipkart/business/GymCentreBusiness.java
@@ -6,16 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GymCentreBusiness implements IGymCentre {
-    List<GymCentre> gymCentreList = new ArrayList<GymCentre>();
-    private String centreID;
-    private String ownerID;
-    private ArrayList<String> slots;
-    private int capacity;
-    private boolean approved;
-    private String city;
-    private String state;
-    private String pincode;
-
+    
     public void createGymCentre(){
         System.out.println("Creating Gym Centre:> ");
     }
