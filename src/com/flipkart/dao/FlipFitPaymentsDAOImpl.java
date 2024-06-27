e4ef6ddf5a51dd4f37139bd5e30f4cbedad9f2c3
pan <pragyapandeystzf@gmail.com>
2024-06-28 01:27:31 +0530

    Payments DAO done

--- src/com/flipkart/dao/FlipFitPaymentsDAOImpl.java
+++ src/com/flipkart/dao/FlipFitPaymentsDAOImpl.java
@@ -14,18 +14,19 @@ public class FlipFitPaymentsDAOImpl implements IFlipFitPaymentsDAO {
 
     public static void main(String[] args) {
         FlipFitPayments FFP = new FlipFitPayments();
+        FlipFitPaymentsDAOImpl FFPDAO = new FlipFitPaymentsDAOImpl();
+
         FFP.setUserID(108);
         FFP.setPaymentType(1);
         FFP.setPaymentInfo("ppan@icici");
+//
+//        FFPDAO.setPaymentInfo(FFP);
+//
+//        FFP.setPaymentType(2);
+//        FFP.setPaymentInfo("123456781298;445");
+//        FFPDAO.setPaymentInfo(FFP);
 
-        FlipFitPaymentsDAOImpl FFPDAO = new FlipFitPaymentsDAOImpl();
-        FFPDAO.setPaymentInfo(FFP);
-
-        FFP.setPaymentType(2);
-        FFP.setPaymentInfo("123456781298;445");
-        FFPDAO.changePaymentInfo(FFP);
-
-//        FFPDAO.deletePaymentInfo(FFP);
+        FFPDAO.deletePaymentInfo(FFP);
     }
     @Override
     public void setPaymentInfo(FlipFitPayments FFP) {
@@ -34,13 +35,13 @@ public class FlipFitPaymentsDAOImpl implements IFlipFitPaymentsDAO {
             Connection con = DriverManager.getConnection(
                     DBConstants.DB_URL,DBConstants.USER,DBConstants.PASSWORD);
 
-            PreparedStatement stmt = con.prepareStatement("INSERT INTO Payments VALUES (?, ?, ?, ?)");
+            PreparedStatement stmt = con.prepareStatement("REPLACE INTO Payments VALUES (?, ?, ?)");
 
 
             // Generate random integers in range 0 to 999
             stmt.setInt(1, FFP.getUserID());
-            stmt.setInt(3, FFP.getPaymentType());
-            stmt.setString(4, FFP.getPaymentInfo());
+            stmt.setInt(2, FFP.getPaymentType());
+            stmt.setString(3, FFP.getPaymentInfo());
 
             int i = stmt.executeUpdate();
             System.out.println( i + " payment info added");
@@ -51,28 +52,7 @@ public class FlipFitPaymentsDAOImpl implements IFlipFitPaymentsDAO {
         }
     }
 
-    @Override
-    public void changePaymentInfo(FlipFitPayments FFP) {
-        try{
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection(
-                    DBConstants.DB_URL,DBConstants.USER,DBConstants.PASSWORD);
 
-            PreparedStatement stmt = con.prepareStatement(("UPDATE Payments SET paymentType = ?, paymentInfo =? WHERE userID = ?"));
-
-
-            stmt.setInt(1, FFP.getPaymentType());
-            stmt.setString(2,FFP.getPaymentInfo());
-            stmt.setInt(7, FFP.getUserID());
-
-            int i = stmt.executeUpdate();
-            System.out.println( i + " payment info updated");
-
-            con.close();
-        } catch(Exception e){
-            System.out.println(e);
-        }
-    }
 
     @Override
     public void deletePaymentInfo(FlipFitPayments FFP) {
