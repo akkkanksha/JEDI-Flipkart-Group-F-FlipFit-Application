709d53940dd7a406faf1cf1fe9be77c66a29418a
isha.rawat <isharawat2142@gmail.com>
2024-06-27 17:18:15 +0530

    Added GymCentredaoimplementation

--- src/com/flipkart/dao/FlipFitGymCentreDAOImpl.java
+++ src/com/flipkart/dao/FlipFitGymCentreDAOImpl.java
@@ -1,4 +1,19 @@
 package com.flipkart.dao;
 
 public class FlipFitGymCentreDAOImpl {
+    public void createGymCentre(){
+        System.out.println("Creating Gym Centre");
+    }
+    public boolean updateGymCentre(){
+        System.out.println("Updating Gym Centre");
+        return true;
+    }
+    public boolean deleteGymCentre(){
+        System.out.println("Deleting Gym Centre");
+        return true;
+
+    }
+    public void viewAvailableSlots(){
+        System.out.println("Viewing Available Slots");
+    }
 }
