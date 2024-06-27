018f49cc79c46a89e0c01e7fb17d6a8d78e30da2
isha.rawat <isharawat2142@gmail.com>
2024-06-28 02:11:09 +0530

    updated

--- src/com/flipkart/client/GymFlipFitAdminMenu.java
+++ src/com/flipkart/client/GymFlipFitAdminMenu.java
@@ -2,9 +2,13 @@ package com.flipkart.client;
 
 import com.flipkart.bean.FlipFitAdmin;
 import com.flipkart.bean.FlipFitAdmin;
+import com.flipkart.bean.FlipFitGymCustomer;
+import com.flipkart.bean.FlipFitGymOwner;
 import com.flipkart.business.FlipFitAdminBusiness;
+import com.flipkart.business.FlipFitGymOwnerBusiness;
 import com.flipkart.business.interfaces.IFlipFitAdmin;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class GymFlipFitAdminMenu {
@@ -12,6 +16,7 @@ public class GymFlipFitAdminMenu {
         Scanner sc=new Scanner(System.in);
         FlipFitAdmin adminUser = new FlipFitAdmin();
         FlipFitAdminBusiness adminService = new FlipFitAdminBusiness(adminUser);
+        FlipFitGymOwnerBusiness ownerService = new FlipFitGymOwnerBusiness();
         System.out.println("1. View Pending Requests");
         System.out.println("2. View Approved Owners");
         System.out.println("3. View all FlipFit Customers");
@@ -21,24 +26,42 @@ public class GymFlipFitAdminMenu {
         switch (choice){
             case 1:{
                 String adminId = "101";
-                adminService.getPendingOwnerList();
-
+                List<FlipFitGymOwner> flipFitGymOwnerList=  adminService.getPendingOwnerList();
+                for(FlipFitGymOwner flipFitGymOwner: flipFitGymOwnerList){
+                    System.out.println("Owner name :" + flipFitGymOwner.getUserName() + " City :" + flipFitGymOwner.getCity());
+                }
                 System.out.println("Type the ownerId of owner you wish to approve");
                 String ownerId = sc.next();
                 // Approve owner
 
 
+
             }
             case 2:{
                 System.out.println("Printing list of Approved Owners");
+                List<FlipFitGymOwner> flipFitGymOwnerList=  adminService.getPendingOwnerList();
+                for(FlipFitGymOwner flipFitGymOwner: flipFitGymOwnerList){
+                    if(flipFitGymOwner.getIsApproved() == true) {
+                        System.out.println("Owner name :" + flipFitGymOwner.getUserName() + " City :" + flipFitGymOwner.getCity());
+                    }
+                }
                 break;
             }
             case 3:{
-                adminService.getUserList();
+                List<FlipFitGymCustomer> customersList = adminService.getUserList();
+                for(FlipFitGymCustomer customers: customersList){
+                    System.out.println("Customer name: " + customers.getUserName() + " City :" + customers.getCity());
+                }
+
                 break;
             }
             case 4:{
                 System.out.println("Printing All Centres of Owner");
+                System.out.println("Type the ownerId of owner for which you wish to view Centres");
+                Scanner in = new Scanner(System.in);
+                int ownerId = in.nextInt();
+
+
                 break;
             }
         }
