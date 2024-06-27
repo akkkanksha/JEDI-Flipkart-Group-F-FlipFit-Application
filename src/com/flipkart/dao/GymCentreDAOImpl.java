65eb5e253232a5c0fec15c0c97a8e934211b042b
pan <pragyapandeystzf@gmail.com>
2024-06-27 10:43:52 +0530

    GymCentreDAO

--- src/com/flipkart/dao/GymCentreDAOImpl.java
+++ src/com/flipkart/dao/GymCentreDAOImpl.java
@@ -1,4 +1,26 @@
 package com.flipkart.dao;
 
-public class GymCentreDAOImpl {
+import com.flipkart.dao.interfaces.IGymCentreDAO;
+
+public class GymCentreDAOImpl implements IGymCentreDAO {
+
+    @Override
+    public void createGymCentre() {
+        
+    }
+
+    @Override
+    public boolean updateGymCentre() {
+        return false;
+    }
+
+    @Override
+    public boolean deleteGymCentre() {
+        return false;
+    }
+
+    @Override
+    public void viewAvailableSlots() {
+
+    }
 }
---
+++ src/com/flipkart/dao/interfaces/IGymCentreDAO.java
@@ -0,0 +1,8 @@
+package com.flipkart.dao.interfaces;
+
+public interface IGymCentreDAO {
+        public void createGymCentre();
+        public boolean updateGymCentre();
+        public boolean deleteGymCentre();
+        public void viewAvailableSlots();
+}
