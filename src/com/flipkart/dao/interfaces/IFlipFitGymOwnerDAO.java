d7e521b1cdacab73aec98235d9fe41764b7ba00b
deependra.singh2 <deependra.singh2@FK1089-215019L.local>
2024-06-28 09:22:02 +0530

    remaining two functions

--- src/com/flipkart/dao/interfaces/IFlipFitGymOwnerDAO.java
+++ src/com/flipkart/dao/interfaces/IFlipFitGymOwnerDAO.java
@@ -3,12 +3,13 @@ package com.flipkart.dao.interfaces;
 import com.flipkart.bean.FlipFitGymCentre;
 import com.flipkart.bean.FlipFitGymCustomer;
 import com.flipkart.bean.FlipFitGymOwner;
+import com.flipkart.bean.FlipFitUser;
 
 import java.util.List;
 
 public interface IFlipFitGymOwnerDAO {
     public boolean addCentre(FlipFitGymCentre centre);
     public List<FlipFitGymCentre> viewCentres(FlipFitGymOwner owner);
-    public void viewFlipFitCustomers();
-    public boolean editDetails();
+    List<FlipFitUser> viewFlipFitCustomers(FlipFitGymCentre centre);
+    public boolean editDetails(FlipFitGymOwner owner);
 }
